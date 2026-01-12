46f3dd4bd210f9e2f667f885f9dfb60ca9997f09
SoumyadipHazari <166205459+SoumyadipHazari@users.noreply.github.com>
2026-01-12 13:10:29 +0530

    added the py file

---
+++ pdfviewer.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+from tkinter import ttk
+
+from tkinter import filedialog as fd
+
+import os
+
+class pdfviewer:
+    def __init__(self, master):
+        pass
+
+root = Tk()
+app = pdfviewer(root)
+root.mainloop()
+
